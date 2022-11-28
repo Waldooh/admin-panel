a380a370f039226d25f08b444bd530e5ac9523ee
Armando Rios <armandorg95@hotmail.com>
2022-11-28 16:32:45 -0600

    adding context logic & setting navbar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { ContextProvider } from "./contexts/ContextProvider";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
+  document.getElementById("root")
+);
 
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(
