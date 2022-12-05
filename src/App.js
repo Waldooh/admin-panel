ebe86c8322abe1689bc1107a81fd12c10210feca
Armando Rios <armandorg95@hotmail.com>
2022-12-05 17:56:32 -0600

    setting pages layout

--- src/App.js
+++ src/App.js
@@ -62,6 +62,8 @@ function App() {
             </div>
 
             <div>
+              <ThemeSettings />
+
               <Routes>
                 {/* Dashboard */}
                 <Route path="/" element={<Ecommerce />} />
